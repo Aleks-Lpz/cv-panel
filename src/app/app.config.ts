d46c6bbe593ce8c244cf1813cd7ebf8c88d8fb87
Aleks <aleks.lpx@gmail.com>
2026-05-06 00:30:10 -0600

    lol2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-// IMPORTANTE: Asegúrate de que provideClientHydration NO esté aquí
+
+// Importaciones necesarias de Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment'; // Ajusta la ruta si es necesario
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // provideClientHydration() <--- ELIMINALO COMPLETAMENTE
+    // ESTOS SON LOS QUE FALTAN O ESTÁN DANDO EL ERROR NG0201
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };

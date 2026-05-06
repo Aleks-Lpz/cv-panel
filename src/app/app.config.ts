c1ff9c978f24eeb59ab51a80cd6fb4dbc8db080b
Aleks <aleks.lpx@gmail.com>
2026-05-05 22:57:12 -0600

    Fix: Final build working with Firebase and Environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore())
-    )
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // Asegúrate de que estén sueltos en el arreglo de providers
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };
